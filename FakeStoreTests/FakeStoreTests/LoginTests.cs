44fd37647ed1f2a595c8bbb960e871e1fc877ed4
Ela <ela.sadel@hotmail.com>
2018-12-01 11:57:53 +0100

    Negative Test Cases added to LoginTests

--- FakeStoreTests/FakeStoreTests/LoginTests.cs
+++ FakeStoreTests/FakeStoreTests/LoginTests.cs
@@ -32,24 +32,118 @@ namespace FakeStoreTests
             driver.FindElement(By.CssSelector("input[id='password']")).SendKeys(password);
             driver.FindElement(By.CssSelector("button[name='login']")).Click();
 
-            var user = "Geralt z Rivii";
+            var expectedUser = "Geralt z Rivii";
+            var accountContent = driver.FindElement(By.CssSelector("div[class='woocommerce-MyAccount-content']")).Text;
 
-            Assert.True(driver.FindElement(By.CssSelector("div[class='woocommerce-MyAccount-content']")).Text.Contains(user));
+            Assert.True(accountContent.Contains(expectedUser), expectedUser + " username was not found after login. \nText after login was: " + accountContent);
 
         }
 
         [Test]
         public void LoginWithUsername()
         {
-            var email = "TestowyUser";
+            var username = "TestowyUser";
             var password = "";
+            driver.FindElement(By.CssSelector("input[id='username']")).SendKeys(username);
+            driver.FindElement(By.CssSelector("input[id='password']")).SendKeys(password);
+            driver.FindElement(By.CssSelector("button[name='login']")).Click();
+
+            var expectedUser = "Geralt z Rivii";
+            var accountContent = driver.FindElement(By.CssSelector("div[class='woocommerce-MyAccount-content']")).Text;
+
+            Assert.True(accountContent.Contains(expectedUser), expectedUser + " username was not found after login. \nText after login was: " + accountContent);
+        }
+
+        [Test]
+        public void UsernameAndWrongPassword()
+        {
+            var username = "TestowyUser";
+            var password = "dummy";
+            driver.FindElement(By.CssSelector("input[id='username']")).SendKeys(username);
+            driver.FindElement(By.CssSelector("input[id='password']")).SendKeys(password);
+            driver.FindElement(By.CssSelector("button[name='login']")).Click();
+
+            var expectedErrorMessage = "BŁĄD: Wprowadzone hasło dla nazwy użytkownika " + username + " nie jest poprawne. Nie pamiętasz hasła?";
+            var actualErrorMessage = driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
+
+            Assert.AreEqual(expectedErrorMessage, actualErrorMessage, 
+                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + actualErrorMessage);
+        }
+
+        [Test]
+        public void EmailAndWrongPassword()
+        {
+            var email = "test@testelka.pl";
+            var password = "dummy";
             driver.FindElement(By.CssSelector("input[id='username']")).SendKeys(email);
             driver.FindElement(By.CssSelector("input[id='password']")).SendKeys(password);
             driver.FindElement(By.CssSelector("button[name='login']")).Click();
 
-            var user = "Geralt z Rivii";
+            var expectedErrorMessage = "BŁĄD: Dla adresu e-mail " + email + " podano nieprawidłowe hasło. Nie pamiętasz hasła?";
+            var actualErrorMessage = driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
 
-            Assert.True(driver.FindElement(By.CssSelector("div[class='woocommerce-MyAccount-content']")).Text.Contains(user));
+            Assert.AreEqual(expectedErrorMessage, actualErrorMessage,
+                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + actualErrorMessage);
+        }
+
+        [Test]
+        public void NonExististentUsername()
+        {
+            var username = "test";
+            var password = "dummy";
+            driver.FindElement(By.CssSelector("input[id='username']")).SendKeys(username);
+            driver.FindElement(By.CssSelector("input[id='password']")).SendKeys(password);
+            driver.FindElement(By.CssSelector("button[name='login']")).Click();
+
+            var expectedErrorMessage = "BŁĄD: Nieprawidłowa nazwa użytkownika. Nie pamiętasz hasła?";
+            var actualErrorMessage = driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
+
+            Assert.AreEqual(expectedErrorMessage, actualErrorMessage,
+                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + actualErrorMessage);
+        }
+
+        [Test]
+        public void NonExistentEmail()
+        {
+            var email = "test@elo.pl";
+            var password = "dummy";
+            driver.FindElement(By.CssSelector("input[id='username']")).SendKeys(email);
+            driver.FindElement(By.CssSelector("input[id='password']")).SendKeys(password);
+            driver.FindElement(By.CssSelector("button[name='login']")).Click();
+
+            var expectedErrorMessage = "BŁĄD: Nieprawidłowy adres e-mail. Nie pamiętasz hasła?";
+            var actualErrorMessage = driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
+
+            Assert.AreEqual(expectedErrorMessage, actualErrorMessage,
+                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + actualErrorMessage);
+        }
+
+        [Test]
+        public void NoUsername()
+        {
+            var password = "dummy";
+            driver.FindElement(By.CssSelector("input[id='password']")).SendKeys(password);
+            driver.FindElement(By.CssSelector("button[name='login']")).Click();
+
+            var expectedErrorMessage = "Błąd: Nazwa użytkownika jest wymagana.";
+            var actualErrorMessage = driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
+
+            Assert.AreEqual(expectedErrorMessage, actualErrorMessage,
+                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + actualErrorMessage);
+        }
+
+        [Test]
+        public void NoPassword()
+        {
+            var username = "test";
+            driver.FindElement(By.CssSelector("input[id='username']")).SendKeys(username);
+            driver.FindElement(By.CssSelector("button[name='login']")).Click();
+
+            var expectedErrorMessage = "BŁĄD: Pole „Hasło” jest puste.";
+            var actualErrorMessage = driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
+
+            Assert.AreEqual(expectedErrorMessage, actualErrorMessage,
+                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + actualErrorMessage);
         }
     }
 }
