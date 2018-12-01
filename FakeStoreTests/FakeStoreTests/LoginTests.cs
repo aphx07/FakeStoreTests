be1bb94e6ae70689839a3dd5b5fbcfeb90c125db
Ela <ela.sadel@hotmail.com>
2018-12-01 11:07:09 +0100

    Login with email and login with username tests added.

---
+++ FakeStoreTests/FakeStoreTests/LoginTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace FakeStoreTests
+{
+    [TestFixture]
+    public class LoginTests
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void DriverSetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://fakestore.testelka.pl/moje-konto/");
+        }
+
+        [TearDown]
+        public void DriverQuitAndClose()
+        {
+            driver.Close();
+            driver.Quit();
+        }        
+
+        [Test]
+        public void LoginWithEmail()
+        {
+            var email = "test@testelka.pl";
+            var password = "";
+            driver.FindElement(By.CssSelector("input[id='username']")).SendKeys(email);
+            driver.FindElement(By.CssSelector("input[id='password']")).SendKeys(password);
+            driver.FindElement(By.CssSelector("button[name='login']")).Click();
+
+            var user = "Geralt z Rivii";
+
+            Assert.True(driver.FindElement(By.CssSelector("div[class='woocommerce-MyAccount-content']")).Text.Contains(user));
+
+        }
+
+        [Test]
+        public void LoginWithUsername()
+        {
+            var email = "TestowyUser";
+            var password = "";
+            driver.FindElement(By.CssSelector("input[id='username']")).SendKeys(email);
+            driver.FindElement(By.CssSelector("input[id='password']")).SendKeys(password);
+            driver.FindElement(By.CssSelector("button[name='login']")).Click();
+
+            var user = "Geralt z Rivii";
+
+            Assert.True(driver.FindElement(By.CssSelector("div[class='woocommerce-MyAccount-content']")).Text.Contains(user));
+        }
+    }
+}
---
+++ FakeStoreTests/FakeStoreTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FakeStoreTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FakeStoreTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49d95d42-3676-427c-bbbb-8af7dfbb2eaf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
