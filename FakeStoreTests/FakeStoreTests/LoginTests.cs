320d31ccd949fb1da1558662c0fc5f51acd31369
Ela <ela.sadel@hotmail.com>
2018-12-08 10:49:02 +0100

    Nunit parameterized tests introduced

--- FakeStoreTests/FakeStoreTests/LoginTests.cs
+++ FakeStoreTests/FakeStoreTests/LoginTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Framework;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -10,132 +11,51 @@ namespace Framework
     public class LoginTests
     {
         IWebDriver Driver { get; set; }
+        static public string Password { get => "m^&vApNUQ#WjuQWUFnj)8G22"; }
+        static public string ExpectedUser { get => "Geralt z Rivii"; }        
+        public string ExpectedErrorMessage { get; set; }
+        public string ActualErrorMessage { get; set; }
+        public string AccountContent { get; set; }
+
         [SetUp]
         public void DriverSetUp()
         {
             var factory = new WebDriverFactory();
-            Driver = factory.Create(BrowserType.Firefox);
-
-            
+            Driver = factory.Create(BrowserType.Firefox);            
             Driver.Navigate().GoToUrl("https://fakestore.testelka.pl/moje-konto/");
         }
 
         [TearDown]
         public void DriverQuitAndClose()
         {
+            ExpectedErrorMessage = null;
+            AccountContent = null;
+            ActualErrorMessage = null;
             Driver.Close();
             Driver.Quit();
-        }        
-
-        [Test]
-        public void LoginWithEmail()
-        {
-            var email = "test@testelka.pl";
-            var password = "";
-            Login(email, password);
-
-            var expectedUser = "Geralt z Rivii";
-            var accountContent = Driver.FindElement(By.CssSelector("div[class='woocommerce-MyAccount-content']")).Text;
-
-            Assert.True(accountContent.Contains(expectedUser), expectedUser + " username was not found after login. \nText after login was: " + accountContent);
-
         }
 
-        [Test]
-        public void LoginWithUsername()
+        [TestCase("test@testelka.pl")]
+        [TestCase("TestowyUser")]
+        public void SuccessfulLogin(string username)
         {
-            var username = "TestowyUser";
-            var password = "";
+            Login(username, Password);
+            AccountContent = Driver.FindElement(By.CssSelector("div[class='woocommerce-MyAccount-content']")).Text;
+            Assert.True(AccountContent.Contains(ExpectedUser), ExpectedUser + " username was not found after login. \nText after login was: " + AccountContent);
+        }
+
+        [TestCase("TestowyUser", "dummy", "BŁĄD: Wprowadzone hasło dla nazwy użytkownika TestowyUser nie jest poprawne. Nie pamiętasz hasła?")]
+        [TestCase("test@testelka.pl", "dummy", "BŁĄD: Dla adresu e-mail test@testelka.pl podano nieprawidłowe hasło. Nie pamiętasz hasła?")]
+        [TestCase("Test", "dummy", "BŁĄD: Nieprawidłowa nazwa użytkownika. Nie pamiętasz hasła?")]
+        [TestCase("elo@testelka.pl", "dummy", "BŁĄD: Nieprawidłowy adres e-mail. Nie pamiętasz hasła?")]
+        [TestCase("", "dummy", "Błąd: Nazwa użytkownika jest wymagana.")]
+        [TestCase("elo@testelka.pl", "", "BŁĄD: Pole „Hasło” jest puste.")]
+        public void UnsuccessfulLogin(string username, string password, string expectedErrorMessage)
+        {
             Login(username, password);
-
-            var expectedUser = "Geralt z Rivii";
-            var accountContent = Driver.FindElement(By.CssSelector("div[class='woocommerce-MyAccount-content']")).Text;
-
-            Assert.True(accountContent.Contains(expectedUser), expectedUser + " username was not found after login. \nText after login was: " + accountContent);
-        }
-
-        [Test]
-        public void UsernameAndWrongPassword()
-        {
-            var username = "TestowyUser";
-            var password = "dummy";
-            Login(username, password);
-
-            var expectedErrorMessage = "BŁĄD: Wprowadzone hasło dla nazwy użytkownika " + username + " nie jest poprawne. Nie pamiętasz hasła?";
-            var actualErrorMessage = Driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
-
-            Assert.AreEqual(expectedErrorMessage, actualErrorMessage, 
-                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + actualErrorMessage);
-        }
-
-        [Test]
-        public void EmailAndWrongPassword()
-        {
-            var email = "test@testelka.pl";
-            var password = "dummy";
-            Login(email, password);
-
-            var expectedErrorMessage = "BŁĄD: Dla adresu e-mail " + email + " podano nieprawidłowe hasło. Nie pamiętasz hasła?";
-            var actualErrorMessage = Driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
-
-            Assert.AreEqual(expectedErrorMessage, actualErrorMessage,
-                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + actualErrorMessage);
-        }
-
-        [Test]
-        public void NonExististentUsername()
-        {
-            var username = "test";
-            var password = "dummy";
-            Login(username, password);
-
-            var expectedErrorMessage = "BŁĄD: Nieprawidłowa nazwa użytkownika. Nie pamiętasz hasła?";
-            var actualErrorMessage = Driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
-
-            Assert.AreEqual(expectedErrorMessage, actualErrorMessage,
-                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + actualErrorMessage);
-        }
-
-        [Test]
-        public void NonExistentEmail()
-        {
-            var email = "test@elo.pl";
-            var password = "dummy";
-            Login(email, password);
-
-            var expectedErrorMessage = "BŁĄD: Nieprawidłowy adres e-mail. Nie pamiętasz hasła?";
-            var actualErrorMessage = Driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
-
-            Assert.AreEqual(expectedErrorMessage, actualErrorMessage,
-                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + actualErrorMessage);
-        }
-
-        [Test]
-        public void NoUsername()
-        {
-            var username = "";
-            var password = "dummy";
-            Login(username, password);
-
-            var expectedErrorMessage = "Błąd: Nazwa użytkownika jest wymagana.";
-            var actualErrorMessage = Driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
-
-            Assert.AreEqual(expectedErrorMessage, actualErrorMessage,
-                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + actualErrorMessage);
-        }
-
-        [Test]
-        public void NoPassword()
-        {
-            var username = "test";
-            var password = "";
-            Login(username, password);
-
-            var expectedErrorMessage = "BŁĄD: Pole „Hasło” jest puste.";
-            var actualErrorMessage = Driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
-
-            Assert.AreEqual(expectedErrorMessage, actualErrorMessage,
-                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + actualErrorMessage);
+            ActualErrorMessage = Driver.FindElement(By.CssSelector("ul[class='woocommerce-error']")).Text;
+            Assert.AreEqual(expectedErrorMessage, ActualErrorMessage,
+                "Error message after failed login was different than expected. \nExpected: " + expectedErrorMessage + "\nActual: " + ActualErrorMessage);
         }
 
         private void Login(string username, string password)
